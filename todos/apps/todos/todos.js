3f6e24161659f3ae5dd86d1d03b09ee8567b469f
Kevin Farst <farst.6@osu.edu>
2011-08-07 00:24:14 -0400

    initial commit

---
+++ todos/apps/todos/todos.js
@@ -0,0 +1,74 @@
+// ==========================================================================
+// Project:   Todos
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+Todos = SC.Application.create();
+
+Todos.Todo = SC.Object.extend({
+  title: null,
+  isDone: false
+});
+
+Todos.CreateTodoView = SC.TextField.extend({
+  insertNewline: function() {
+    var value = this.get('value');
+ 
+    if (value) {
+      Todos.todoListController.createTodo(value);
+      this.set('value', '');
+    }
+  }
+});
+
+Todos.MarkDoneView = SC.Checkbox.extend({
+  titleBinding: '.parentView.content.title',
+  valueBinding: '.parentView.content.isDone'
+});
+
+Todos.StatsView = SC.TemplateView.extend({
+  remainingBinding: 'Todos.todoListController.remaining',
+ 
+  displayRemaining: function() {
+    var remaining = this.get('remaining');
+    return remaining + (remaining === 1 ? " item" : " items");
+  }.property('remaining')
+});
+
+SC.ready(function() {
+  Todos.mainPane = SC.TemplatePane.append({
+    layerId: 'todos',
+    templateName: 'todos'
+  });
+});
+
+Todos.todoListController = SC.ArrayController.create({
+  // Initialize the array controller with an empty array.
+  content: [],
+
+  // Creates a new todo with the passed title, then adds it
+  // to the array.
+  createTodo: function(title) {
+    var todo = Todos.Todo.create({ title: title });
+    this.pushObject(todo);
+  },
+  
+  clearCompletedTodos: function() {
+    this.filterProperty('isDone', true).forEach(this.removeObject, this);
+  },
+
+  remaining: function() {
+    return this.filterProperty('isDone', false).get('length');
+  }.property('@each.isDone'),
+
+  allAreDone: function(key, value) {
+    if (value !== undefined) {
+      this.setEach('isDone', value);
+ 
+      return value;
+    } else {
+      return this.get('length') && this.everyProperty('isDone', true);
+    }
+  }.property('@each.isDone')
+});
